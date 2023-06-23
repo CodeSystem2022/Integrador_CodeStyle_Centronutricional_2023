7981d64f3782353b94be0b8b3c5e6ed4dcb94539
Ayoroa Juan Pablo <pabloayoroa23@gmail.com>
2023-06-22 21:22:07 -0300

    se agrega getters y setters y classmethod calcularIMC

--- Backend/CentroNutricional/paciente.py
+++ Backend/CentroNutricional/paciente.py
@@ -5,7 +5,7 @@ class Paciente:
         self._apellido = apellido
         self._peso = peso
         self._altura = altura
-        self._imc = Paciente
+        self._imc = Paciente.calcularIMC(peso,altura)
         
     def __str__(self):
         return f'''
@@ -19,3 +19,49 @@ class Paciente:
     '''
     
     #setters y getters
+    
+    @property
+    def id_paciente(self):
+        return self._id_paciente
+    
+    id_paciente.setter
+    def id_paciente(self, id_paciente):
+        self._id_paciente = id_paciente
+    
+    
+    @property
+    def nombre(self):
+        return self._nombre
+    
+    @nombre.setter
+    def nombre(self, nombre):
+        self._nombre = nombre
+        
+    @property
+    def peso(self):
+        return self._peso
+    
+    @peso.setter
+    def peso(self, peso):
+        self._peso = peso
+    
+    @property
+    def altura(self):
+        return self._altura
+    
+    @altura.setter
+    def altura(self, altura):
+        self._altura = altura
+    
+    
+    #calculamos el IMC(indice de masa corporal) a traves de un classmethod y un getter
+    @classmethod
+    
+    def calcularIMC(cls, peso, altura):
+        imc = round(peso/(altura*altura),2)# con round hacemos un redondeo a dos decimales
+        return imc
+    
+    @property
+    def imc(self):
+        return self._imc
+        
